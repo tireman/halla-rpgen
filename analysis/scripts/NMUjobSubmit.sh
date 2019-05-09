7bfbe74c3f8d1895899a765745f87e6a23f2e449
tireman <tireman@nmu.edu>
2019-05-09 08:55:37 -0400

    Added lines for charge/neutral processing and analysis commands. Changed to represent paths for RP-gen code.

--- analysis/scripts/NMUjobSubmit.sh
+++ analysis/scripts/NMUjobSubmit.sh
@@ -6,25 +6,29 @@ for ((i=$1; i<=$2; i++))
 do
     export JOBNUMBER=$i
 
-	#export HistoOutputDir=$NPOLWORKDIR/histos
+	export HistoOutputDir=$NPOLWORKDIR/histos
 	#$BUILD_DIR/../analysis/NpolAnalysis 1>$NPOLWORKDIR/dumpFiles/${NPOLBASENAME}Analysis1_$i.out 2>$NPOLWORKDIR/dumpFiles/${NPOLBASENAME}Analysis1_$i.err
 	
-	#export CHARGE_TYPE=All
-	#export HistoOutputDir=$NPOLWORKDIR/$CHARGE_TYPE\Particles/histos
+	export CHARGE_TYPE=All
+	export HistoOutputDir=$NPOLWORKDIR/$CHARGE_TYPE\Particles/histos
 	#$BUILD_DIR/../analysis2/NpolProcessEvents
-
-	#export CHARGE_TYPE=Neutral
-	#export HistoOutputDir=$NPOLWORKDIR/$CHARGE_TYPE\Particles/histos
-	#$BUILD_DIR/../analysis2/NpolProcessEvents
-
-	#export CHARGE_TYPE=Charged
-	#export HistoOutputDir=$NPOLWORKDIR/$CHARGE_TYPE\Particles/histos
-	#$BUILD_DIR/../analysis2/NpolProcessEvents
-
+	export RatesOutputDir=$NPOLWORKDIR/$CHARGE_TYPE\Particles/rates
 	$BUILD_DIR/../analysis3/NpolRates
 
+	export CHARGE_TYPE=Neutral
+	export HistoOutputDir=$NPOLWORKDIR/$CHARGE_TYPE\Particles/histos
+	#$BUILD_DIR/../analysis2/NpolProcessEvents
+	export RatesOutputDir=$NPOLWORKDIR/$CHARGE_TYPE\Particles/rates
+	$BUILD_DIR/../analysis3/NpolRates
+
+	export CHARGE_TYPE=Charged
+	export HistoOutputDir=$NPOLWORKDIR/$CHARGE_TYPE\Particles/histos
+	#$BUILD_DIR/../analysis2/NpolProcessEvents
+	export RatesOutputDir=$NPOLWORKDIR/$CHARGE_TYPE\Particles/rates
+	$BUILD_DIR/../analysis3/NpolRates
+	
+	#$BUILD_DIR/../analysis3/NpolRates
 	
 	#root -l -q $BUILD_DIR/../../root_macros/ProcessElectrons.cxx
 	
 done    
-
